7225051f870915423d66baff249f16d7a590cd5d
victorpropython <victorchibuzor3109@gmail.com>
2023-03-28 16:26:15 -0700

    for main.h and _printf.c

--- main.h
+++ main.h
@@ -17,6 +17,16 @@ int _printf(const char *arrange, int i, va_list varlist, char bufferof[], int fl
 int handle_print(const char *fmt, int *i,va_list varlist, char buffer[], int flags, int width, int precision, int size);
 
 
+/* width handler */
+
+int handle_write_char(char c, char buffer[],int flags, int width, int precision, int size);
+int write_number(int is_positive, int index, char buffer[], int flags, int width, int precision, int size);
+int write_num(int index, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
+int write_pointer(char buffer[], int index, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int write_unsgnd(int is_negative, int index, char buffer[], int flags, int width, int precision, int size);
+
+
+
 
 
 #endif

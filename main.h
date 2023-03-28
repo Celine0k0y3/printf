a9895de7002bfda308b96d2a0767b7e597d7943c
victorpropython <victorchibuzor3109@gmail.com>
2023-03-28 16:39:15 -0700

    for main.h and _printf.c

--- main.h
+++ main.h
@@ -25,6 +25,34 @@ int write_num(int index, char bff[], int flags, int width, int precision, int le
 int write_pointer(char buffer[], int index, int length, int width, int flags, char padd, char extra_c, int padd_start);
 int write_unsgnd(int is_negative, int index, char buffer[], int flags, int width, int precision, int size);
 
+/****************** FUNCTIONS ******************/
+/* Funtions to print chars and strings */
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
+/* Functions to print numbers */
+int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_unsigned(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[],char buffer[], int flags, char flag_ch, int width, int precision, int size);
+/* Function to print non printable characters */
+int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size);
+/* Funcion to print memory address */
+int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size);
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list varlist);
+int get_precision(const char *format, int *i, va_list varlist);
+int get_size(const char *format, int *i);
+/*Function to print string in reverse*/
+int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size);
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size);
+
+
 
 
 

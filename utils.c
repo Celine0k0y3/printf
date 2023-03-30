6dc7a12c1e0ae201d596f247b7c2d8476a025d69
Victor chibuike <90730919+Victorpropython@users.noreply.github.com>
2023-03-30 21:36:47 +0000

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable char
+ * is_printable - Evaluates if a variable is printable char
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
